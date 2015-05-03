3f124da4f4035232e615de6a6e180a234dcec481
Darrell Cohn <darrellcohn@gmail.com>
2015-05-03 12:57:48 -0700

    Modify entire users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,38 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!
+  before_filter :authenticate_user!
+  after_action :verify_authorized
 
-	def new
-		@user = User.new
-	end
+  def index
+    @users = User.all
+    authorize User
+  end
 
-	def create
-		@user = User.create(user_params)
-		redirect_to user_path(@user.id)
-	end
+  def show
+    @user = User.find(params[:id])
+    authorize @user
+  end
 
-	def show
-	end
+  def update
+    @user = User.find(params[:id])
+    authorize @user
+    if @user.update_attributes(secure_params)
+      redirect_to users_path, :notice => "User updated."
+    else
+      redirect_to users_path, :alert => "Unable to update user."
+    end
+  end
 
+  def destroy
+    user = User.find(params[:id])
+    authorize user
+    user.destroy
+    redirect_to users_path, :notice => "User deleted."
+  end
 
-	private
+  private
 
-	def user_params
-	    params.require(:user).permit(:email, :password, :password_confirmation)
-	end
+  def secure_params
+    params.require(:user).permit(:role)
+  end
 
-
-	end
-	end
+end
