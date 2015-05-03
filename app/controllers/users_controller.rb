932a63af067e12ab3c65e002247ff40f3b8bc3f6
Darrell Cohn <darrellcohn@gmail.com>
2015-05-03 10:39:35 -0700

    Add Foundation gem to Gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,25 @@
 class UsersController < ApplicationController
+	before_action :authenticate_user!
+
 	def new
-    @user = User.new
-  end
+		@user = User.new
+	end
 
-  def create
-    @user = User.create(user_params)
-    redirect_to user_path(@user.id)
-  end
+	def create
+		@user = User.create(user_params)
+		redirect_to user_path(@user.id)
+	end
 
-  def show
-  end
-
-
-  private
-
-    def user_params
-        params.require(:user).permit(:email, :password, :password_confirmation)
-    end
+	def show
+	end
 
 
-end
-end
+	private
+
+	def user_params
+	    params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+
+
+	end
+	end
